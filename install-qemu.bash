604aef9e07ff9a8324853556918e0c47e0d88c6b
Strahinja Jankovic <strahinja.p.jankovic@gmail.com>
2024-12-23 21:59:06 +0100

    Update for Ubuntu 24.04
    Signed-off-by: Strahinja Jankovic <strahinja.p.jankovic@gmail.com>

--- install-qemu.bash
+++ install-qemu.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-QEMU_VER=8.2.2
-TOOLCHAIN_VER=13.2.rel1
-UBOOT_VER=2024.01
-LINUX_VER=6.8.1
-NR_PROC=16
+QEMU_VER=9.1.1
+TOOLCHAIN_VER=13.3.rel1
+UBOOT_VER=2024.10
+LINUX_VER=6.11.8
+UBUNTU_VER=24.04.1
+NR_PROC=8
 
 # Global definitions
 PROJDIR_PATH=$PWD
@@ -12,70 +13,68 @@ ENVFILE=env.sh
 
 set -e
 
-if [ `id -u` -eq 0 ]; then
-   echo "Must run as regular user"
-   exit
+if [ $(id -u) -eq 0 ]; then
+  echo "Must run as regular user"
+  exit
 fi
 
-function check_existing
-{
-    if [ "$1" = "clean" ]; then
-        echo "Deleting old installation"
-        rm -rf ${PROJDIR_PATH}/qemu
-        rm -rf ${PROJDIR_PATH}/u-boot
-        rm -rf ${PROJDIR_PATH}/linux
-        rm -rf ${PROJDIR_PATH}/toolchain
-        rm -rf $ENVFILE
-    else
-        if [ -f $ENVFILE ]; then
-            echo "Detected previous installation, aborting."
-            echo "Pass 'clean' if you want to refresh installation."
-            exit
-        fi
+function check_existing {
+  if [ "$1" = "clean" ]; then
+    echo "Deleting old installation"
+    rm -rf ${PROJDIR_PATH}/qemu
+    rm -rf ${PROJDIR_PATH}/u-boot
+    rm -rf ${PROJDIR_PATH}/linux
+    rm -rf ${PROJDIR_PATH}/toolchain
+    rm -rf $ENVFILE
+  else
+    if [ -f $ENVFILE ]; then
+      echo "Detected previous installation, aborting."
+      echo "Pass 'clean' if you want to refresh installation."
+      exit
     fi
+  fi
 }
 
-function build_qemu
-{
-    echo "Installing prerequisites ..."
-# Installing prerequisites
-    sudo apt -y install git libglib2.0-dev libfdt-dev libpixman-1-dev \
-        zlib1g-dev libnfs-dev libiscsi-dev git-email libaio-dev \
-        libbluetooth-dev libbrlapi-dev libbz2-dev libcap-dev \
-        libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev libibverbs-dev \
-        libjpeg8-dev libncurses5-dev libnuma-dev librbd-dev \
-        librdmacm-dev libsasl2-dev libsdl2-dev libseccomp-dev \
-        libsnappy-dev libssh2-1-dev libvde-dev libvdeplug-dev \
-        libxen-dev liblzo2-dev valgrind xfslibs-dev kpartx libssl-dev \
-        net-tools python3-sphinx python3-sphinx-rtd-theme libsdl2-image-dev \
-        flex bison libgmp3-dev libmpc-dev device-tree-compiler u-boot-tools \
-        bc git libncurses5-dev lzop make tftpd-hpa uml-utilities \
-        nfs-kernel-server swig ninja-build libusb-1.0-0-dev python3-venv \
-        python3-setuptools python3-dev fdisk
-    echo "                         ... done"
+function build_qemu {
+  echo "Installing prerequisites ..."
+  # Installing prerequisites
+  sudo apt -y install git libglib2.0-dev libfdt-dev libpixman-1-dev \
+    zlib1g-dev libnfs-dev libiscsi-dev git-email libaio-dev \
+    libbluetooth-dev libbrlapi-dev libbz2-dev libcap-dev \
+    libcap-ng-dev libcurl4-gnutls-dev libgtk-3-dev libibverbs-dev \
+    libjpeg8-dev libncurses5-dev libnuma-dev librbd-dev \
+    librdmacm-dev libsasl2-dev libsdl2-dev libseccomp-dev \
+    libsnappy-dev libssh2-1-dev libvde-dev libvdeplug-dev \
+    libxen-dev liblzo2-dev valgrind xfslibs-dev kpartx libssl-dev \
+    net-tools python3-sphinx python3-sphinx-rtd-theme libsdl2-image-dev \
+    flex bison libgmp3-dev libmpc-dev device-tree-compiler u-boot-tools \
+    bc git libncurses5-dev lzop make tftpd-hpa uml-utilities \
+    nfs-kernel-server swig ninja-build libusb-1.0-0-dev python3-venv \
+    python3-setuptools python3-dev fdisk libgnutls28-dev iptables dosfstools
+  echo "                         ... done"
 
-    echo "Downloading QEMU ..."
-    wget -c https://download.qemu.org/qemu-${QEMU_VER}.tar.xz
-    tar xf qemu-${QEMU_VER}.tar.xz
-    mv qemu-${QEMU_VER} qemu
-    echo "                 ... done"
+  echo "Downloading QEMU ..."
+  wget -c https://download.qemu.org/qemu-${QEMU_VER}.tar.xz
+  tar xf qemu-${QEMU_VER}.tar.xz
+  mv qemu-${QEMU_VER} qemu
+  echo "                 ... done"
 
-    echo "Building QEMU ..."
-    pushd ${PROJDIR_PATH}/qemu
-    mkdir -p bin/arm
-    pushd bin/arm
-    ../../configure --target-list=arm-softmmu \
-                    --enable-sdl \
-                    --enable-tools \
-                    --enable-fdt \
-                    --enable-libnfs \
-                    --audio-drv-list=alsa
-    make -j${NR_PROC}
+  echo "Building QEMU ..."
+  pushd ${PROJDIR_PATH}/qemu
+  mkdir -p build
+  pushd build
+  ../configure --target-list=arm-softmmu \
+    --enable-sdl \
+    --enable-tools \
+    --enable-fdt \
+    --enable-libnfs \
+    --audio-drv-list=alsa
+  make -j${NR_PROC}
 
-    export PATH=${PWD}/arm-softmmu:$PWD:$PATH
+  export PATH=${PWD}/arm-softmmu:$PWD:$PATH
 
-    # Update env.sh
-tee ${PROJDIR_PATH}/${ENVFILE} << EOF
+  # Update env.sh
+  tee ${PROJDIR_PATH}/${ENVFILE} <<EOF
 # global defines
 export PROJDIR_PATH=${PROJDIR_PATH}
 
@@ -86,87 +85,84 @@ export QEMU_TOOLS_PATH=${PWD}
 # PATH update
 export PATH=\${QEMU_PATH}:\${QEMU_TOOLS_PATH}:\$PATH
 EOF
-    popd; popd
-    echo "               ... done"
+  popd
+  popd
+  echo "               ... done"
 }
 
-function get_toolchain
-{
-    echo "Getting toolchain ..."
-    wget -c https://developer.arm.com/-/media/Files/downloads/gnu/${TOOLCHAIN_VER}/binrel/arm-gnu-toolchain-${TOOLCHAIN_VER}-x86_64-arm-none-linux-gnueabihf.tar.xz
-    mkdir -p toolchain
-    tar xf arm-gnu-toolchain-${TOOLCHAIN_VER}-x86_64-arm-none-linux-gnueabihf.tar.xz -C toolchain --strip-components=1
+function get_toolchain {
+  echo "Getting toolchain ..."
+  wget -c https://developer.arm.com/-/media/Files/downloads/gnu/${TOOLCHAIN_VER}/binrel/arm-gnu-toolchain-${TOOLCHAIN_VER}-x86_64-arm-none-linux-gnueabihf.tar.xz
+  mkdir -p toolchain
+  tar xf arm-gnu-toolchain-${TOOLCHAIN_VER}-x86_64-arm-none-linux-gnueabihf.tar.xz -C toolchain --strip-components=1
 
-    export PATH=${PWD}/toolchain/bin:$PATH
+  export PATH=${PWD}/toolchain/bin:$PATH
 
-    # Update env.sh
-tee -a ${PROJDIR_PATH}/${ENVFILE} << EOF
+  # Update env.sh
+  tee -a ${PROJDIR_PATH}/${ENVFILE} <<EOF
 export PATH=\${PROJDIR_PATH}/toolchain/bin:\$PATH
 EOF
-    echo "                  ... done"
+  echo "                  ... done"
 }
 
-function build_uboot
-{
-    echo "Building U-Boot ..."
-    # Download
-    wget -c https://ftp.denx.de/pub/u-boot/u-boot-${UBOOT_VER}.tar.bz2
-    tar xf u-boot-${UBOOT_VER}.tar.bz2
-    mv u-boot-${UBOOT_VER} u-boot
+function build_uboot {
+  echo "Building U-Boot ..."
+  # Download
+  wget -c https://ftp.denx.de/pub/u-boot/u-boot-${UBOOT_VER}.tar.bz2
+  tar xf u-boot-${UBOOT_VER}.tar.bz2
+  mv u-boot-${UBOOT_VER} u-boot
 
-    # Build
-    pushd u-boot
-    make CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard Cubieboard_defconfig
-    make CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard -j${NR_PROC}
-    # tee -a build_cubieboard/.config << EOF
-# CONFIG_OF_EMBED=y
-# EOF
-    # make CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard -j${NR_PROC}
-    popd
+  # Build
+  pushd u-boot
+  make CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard Cubieboard_defconfig
+  make CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard -j${NR_PROC}
+  # tee -a build_cubieboard/.config << EOF
+  # CONFIG_OF_EMBED=y
+  # EOF
+  # make CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard -j${NR_PROC}
+  popd
 
-tee -a ${PROJDIR_PATH}/${ENVFILE} << EOF
+  tee -a ${PROJDIR_PATH}/${ENVFILE} <<EOF
 # U-Boot file path
 export UBOOT=\${PROJDIR_PATH}/u-boot/build_cubieboard/u-boot
 export UBOOT_SPL=\${PROJDIR_PATH}/u-boot/build_cubieboard/u-boot-sunxi-with-spl.bin
 EOF
 
-    echo "                ... done"
+  echo "                ... done"
 }
 
-function build_linux
-{
-    echo "Building linux ..."
-    # Download
-    wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${LINUX_VER}.tar.xz
-    tar xf linux-${LINUX_VER}.tar.xz
-    mv linux-${LINUX_VER} linux
+function build_linux {
+  echo "Building linux ..."
+  # Download
+  wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${LINUX_VER}.tar.xz
+  tar xf linux-${LINUX_VER}.tar.xz
+  mv linux-${LINUX_VER} linux
 
-    # Build
-    pushd linux
-    make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard multi_v7_defconfig
-    make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard -j${NR_PROC}
-    popd
+  # Build
+  pushd linux
+  make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard multi_v7_defconfig
+  make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- O=build_cubieboard -j${NR_PROC}
+  popd
 
-tee -a ${PROJDIR_PATH}/${ENVFILE} << EOF
+  tee -a ${PROJDIR_PATH}/${ENVFILE} <<EOF
 # Linux files path
 export ZIMAGE=\${PROJDIR_PATH}/linux/build_cubieboard/arch/arm/boot/zImage
 export DTB=\${PROJDIR_PATH}/linux/build_cubieboard/arch/arm/boot/dts/allwinner/sun4i-a10-cubieboard.dtb
 EOF
 
-    echo "               ... done"
+  echo "               ... done"
 }
 
-function get_ubuntu
-{
-    echo "Getting Ubuntu filesystem ..."
-    # Download
-    wget -c https://cdimage.ubuntu.com/ubuntu-base/releases/22.04.3/release/ubuntu-base-22.04.3-base-armhf.tar.gz
+function get_ubuntu {
+  echo "Getting Ubuntu filesystem ..."
+  # Download
+  wget -c https://cdimage.ubuntu.com/ubuntu-base/releases/${UBUNTU_VER}/release/ubuntu-base-${UBUNTU_VER}-base-armhf.tar.gz
 
-tee -a ${PROJDIR_PATH}/${ENVFILE} << EOF
+  tee -a ${PROJDIR_PATH}/${ENVFILE} <<EOF
 # Ubuntu rootfs archive
-export UBUNTU_BASE=\${PROJDIR_PATH}/ubuntu-base-22.04.3-base-armhf.tar.gz
+export UBUNTU_BASE=\${PROJDIR_PATH}/ubuntu-base-${UBUNTU_VER}-base-armhf.tar.gz
 EOF
-    echo "                          ... done"
+  echo "                          ... done"
 }
 
 check_existing $1
--- prepare-qemu.bash
+++ prepare-qemu.bash
@@ -7,27 +7,32 @@ ROOTFS_TMP=/run/tmp/rootfs
 BOOT_MNT=/run/mount/boot
 ROOT_MNT=/run/mount/rootfs
 
-if [ `id -u` -eq 0 ]; then
-    echo "Must run as regular user"
-    exit
+if [ $(id -u) -eq 0 ]; then
+  echo "Must run as regular user"
+  exit
 fi
 
-function make_rootfs
-{
-    sudo apt install -y qemu-user-static binfmt-support
-    sudo mkdir -p $ROOTFS_TMP
-    sudo tar xzvf $UBUNTU_BASE -C $ROOTFS_TMP
+function make_rootfs {
+  sudo apt install -y qemu-user-static binfmt-support
+  sudo mkdir -p $ROOTFS_TMP
+  sudo tar xzvf $UBUNTU_BASE -C $ROOTFS_TMP
 
-    # Prepare chroot
-    sudo tee -a ${ROOTFS_TMP}/etc/resolv.conf << "EOF"
+  # Configure binfmt_misc
+  if [ ! -f /proc/sys/fs/binfmt_misc/qemu-arm ]; then
+    sudo sh ${QEMU_PATH}/../../scripts/qemu-binfmt-conf.sh
+    sudo cp $(which qemu-arm-static) ${ROOTFS_TMP}/usr/local/bin/qemu-arm
+  fi
+
+  # Prepare chroot
+  sudo tee -a ${ROOTFS_TMP}/etc/resolv.conf <<"EOF"
 nameserver 8.8.8.8
 EOF
-    sudo mount --bind /dev ${ROOTFS_TMP}/dev
-    sudo mount --bind /dev/pts ${ROOTFS_TMP}/dev/pts
-    sudo mount --bind /proc ${ROOTFS_TMP}/proc
+  sudo mount --bind /dev ${ROOTFS_TMP}/dev
+  sudo mount --bind /dev/pts ${ROOTFS_TMP}/dev/pts
+  sudo mount --bind /proc ${ROOTFS_TMP}/proc
 
-    # Prepare commands in chroot
-    sudo tee -a ${ROOTFS_TMP}/startup.sh << "EOF"
+  # Prepare commands in chroot
+  sudo tee -a ${ROOTFS_TMP}/startup.sh <<"EOF"
 # Prepare image
 apt update
 DEBIAN_FRONTEND="noninteractive" TZ="Europe/Stockholm" apt install -y tzdata
@@ -41,6 +46,8 @@ apt install -y --no-install-recommends \
     iputils-ping \
     vim \
     sudo
+# Enable systemd-networkd
+systemctl enable systemd-networkd
 # Add user ubuntu with password temppwd
 useradd -G sudo -m -s /bin/bash ubuntu
 echo ubuntu:temppwd | chpasswd
@@ -53,14 +60,14 @@ apt clean
 rm /var/lib/apt/lists/ports.ubuntu.com*
 exit
 EOF
-    sudo chmod +x ${ROOTFS_TMP}/startup.sh
-    # Enter chroot
-    sudo chroot ${ROOTFS_TMP} /bin/bash -c /startup.sh
-    sudo rm ${ROOTFS_TMP}/startup.sh
-    # Setup network
-    sudo tee -a ${ROOTFS_TMP}/etc/systemd/network/20-wired.network << "EOF"
+  sudo chmod +x ${ROOTFS_TMP}/startup.sh
+  # Enter chroot
+  sudo chroot ${ROOTFS_TMP} /bin/bash -c /startup.sh
+  sudo rm ${ROOTFS_TMP}/startup.sh
+  # Setup network
+  sudo tee -a ${ROOTFS_TMP}/etc/systemd/network/20-wired.network <<"EOF"
 [Match]
-Name=eth0
+Name=end0
 
 [Network]
 Address=192.168.123.101/24
@@ -68,74 +75,73 @@ Gateway=192.168.123.1
 DNS=8.8.8.8
 EOF
 
-    # Cleanup
-    sudo umount ${ROOTFS_TMP}/proc
-    sudo umount ${ROOTFS_TMP}/dev/pts
-    sudo umount ${ROOTFS_TMP}/dev
+  # Cleanup
+  sudo umount ${ROOTFS_TMP}/proc
+  sudo umount ${ROOTFS_TMP}/dev/pts
+  sudo umount ${ROOTFS_TMP}/dev
 
-    # Archive
-    sudo tar cJvfp ubuntu-minimal-24.04.tar.xz -C ${ROOTFS_TMP} .
+  # Archive
+  sudo tar cJvfp ubuntu-minimal-24.04.tar.xz -C ${ROOTFS_TMP} .
 }
 
-function prepare_sd_image
-{
-    echo "Preparing SD card image ..."
+function prepare_sd_image {
+  echo "Preparing SD card image ..."
 
-    cd $PROJDIR_PATH
-    qemu-img create $SD_FILE 4G
+  cd $PROJDIR_PATH
+  qemu-img create $SD_FILE 4G
 
-tee -a ${PROJDIR_PATH}/$ENVFILE << EOF
+  tee -a ${PROJDIR_PATH}/$ENVFILE <<EOF
 # SD card image
 export SD_IMG=\${PROJDIR_PATH}/${SD_FILE}
 EOF
 
-    # Partition
-    sfdisk ./${SD_FILE} << EOF
+  # Partition
+  sfdisk ./${SD_FILE} <<EOF
 ,64M,c,*
 ,,L,
 EOF
 
-    # Format
-    loop_nr=$(sudo kpartx -av $SD_FILE | grep -Po 'loop.+?(?=p)' | tail -1)
-    sleep 1
-    sudo mkfs.vfat -F 32 -n "BOOT" /dev/mapper/${loop_nr}p1
-    sudo mkfs.ext4 -L rootfs /dev/mapper/${loop_nr}p2
+  # Format
+  loop_nr=$(sudo kpartx -av $SD_FILE | grep -Po 'loop.+?(?=p)' | tail -1)
+  sleep 1
+  sudo mkfs.vfat -F 32 -n "BOOT" /dev/mapper/${loop_nr}p1
+  sudo mkfs.ext4 -L rootfs /dev/mapper/${loop_nr}p2
 
-    # Copy boot files
-    tee -a boot.cmd << "EOF"
+  # Copy boot files
+  tee -a boot.cmd <<"EOF"
 fatload mmc 0:1 42000000 zImage
 fatload mmc 0:1 43000000 sun4i-a10-cubieboard.dtb
 setenv bootargs "console=ttyS0 root=/dev/mmcblk0p2 rw"
 bootz 0x42000000 - 0x43000000
 EOF
-    mkimage -C none -A arm -T script -d boot.cmd boot.scr
-    sudo mkdir -p $BOOT_MNT
-    sudo mount /dev/mapper/${loop_nr}p1 $BOOT_MNT
-    sudo cp $ZIMAGE $BOOT_MNT
-    sudo cp $DTB $BOOT_MNT
-    sudo cp boot.scr $BOOT_MNT
-    sudo umount $BOOT_MNT
+  mkimage -C none -A arm -T script -d boot.cmd boot.scr
+  sudo mkdir -p $BOOT_MNT
+  sudo mount /dev/mapper/${loop_nr}p1 $BOOT_MNT
+  sudo cp $ZIMAGE $BOOT_MNT
+  sudo cp $DTB $BOOT_MNT
+  sudo cp boot.scr $BOOT_MNT
+  sudo umount $BOOT_MNT
 
-    make_rootfs
+  make_rootfs
 
-    # Copy rootfs files
-    sudo mkdir -p $ROOT_MNT
-    sudo mount /dev/mapper/${loop_nr}p2 $ROOT_MNT
-    sudo tar xfvp ubuntu-minimal-24.04.tar.xz -C $ROOT_MNT
+  # Copy rootfs files
+  sudo mkdir -p $ROOT_MNT
+  sudo mount /dev/mapper/${loop_nr}p2 $ROOT_MNT
+  sudo tar xfvp ubuntu-minimal-24.04.tar.xz -C $ROOT_MNT
 
-    # Copy kernel modules
-    pushd ${PROJDIR_PATH}/linux/build_cubieboard
-    sudo make ARCH=arm INSTALL_MOD_PATH=$ROOT_MNT modules_install
-    sync
-    popd
+  # Copy kernel modules
+  pushd ${PROJDIR_PATH}/linux/build_cubieboard
+  sudo make ARCH=arm INSTALL_MOD_PATH=$ROOT_MNT modules_install
+  sync
+  popd
 
-    sudo umount $ROOT_MNT
-    sudo kpartx -d $PROJDIR_PATH/$SD_FILE
+  sudo umount $ROOT_MNT
+  sudo kpartx -d $PROJDIR_PATH/$SD_FILE
 
-    # Copy U-Boot
-    dd if=$UBOOT_SPL of=$SD_FILE bs=1024 seek=8 conv=notrunc
+  # Copy U-Boot
+  dd if=$UBOOT_SPL of=$SD_FILE bs=1024 seek=8 conv=notrunc
 
-    echo "                     ... done"
+  echo "                     ... done"
 }
 
 source $ENVFILE
